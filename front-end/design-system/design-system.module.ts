583142c9ec080693aec66fc9574a99ecb6fca85b
siravor <luis.sr@aluno.ifsc.edu.br>
2020-06-11 14:22:09 -0300

    estrutura

---
+++ front-end/design-system/design-system.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [ButtonComponent],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+
+  ]
+})
+export class DesignSystemModule { }
--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DesignSystemModule } from '../../design-system/design-system.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DesignSystemModule
   ],
   providers: [],
   bootstrap: [AppComponent]
