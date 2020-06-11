5b592255f83650bb38cd3b93810000554e832817
ciochetta <luisfelipeciochetta@gmail.com>
2020-06-11 16:38:08 -0300

    fix db ?

--- api/db.js
+++ api/db.js
@@ -2,12 +2,12 @@ const MongoClient = require('mongodb').MongoClient;
 
 const url = process.env.MONGO_URL || "mongodb+srv://Ademir:9Dyo5zhppA2JS6mH@cluster0-fmuw8.mongodb.net/PixelProject?retryWrites=true&w=majority";
 
-let client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true   });
+let client = new MongoClient(url, { useNewUrlParser: true });
 let connection;
 let database;
 
 async function init_db(){
-  client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true   });
+  client = new MongoClient(url, { useNewUrlParser: true });
 
   connection = await client.connect();
   database = await client.db('PixelProject');
