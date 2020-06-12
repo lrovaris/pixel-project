9602a32f1c62b9878ce8aa601777b626eed08e25
siravor <luis.sr@aluno.ifsc.edu.br>
2020-06-12 17:32:14 -0300

    update

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {IpcService} from "./services/ipc.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'front-end';
+
+  constructor(private ipc: IpcService) {
+
+  }
+
+  ngOnInit() {
+    this.ipc.send('load-metadata');
+  }
+
 }
---
+++ front-end/src/app/services/ipc.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { IpcRenderer } from 'electron';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IpcService {
+
+  private ipc: IpcRenderer | undefined = void 0;
+
+  constructor() {
+    if (window.require) {
+      try {
+        this.ipc = window.require('electron').ipcRenderer;
+      } catch (e) {
+        throw e;
+      }
+    } else {
+      console.warn('Electron\'s IPC was not loaded');
+    }
+  }
+
+  public on(channel: string, listener): void {
+    if (!this.ipc) {
+      return;
+    }
+    this.ipc.on(channel, listener);
+  }
+
+  public send(channel: string, ...args): void {
+
+    if (!this.ipc) {
+      console.log(this.ipc);
+      return;
+    }
+    this.ipc.send(channel, ...args);
+  }
+
+
+}
