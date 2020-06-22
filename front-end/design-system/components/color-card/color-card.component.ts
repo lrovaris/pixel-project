e69190a6eb3dbc4f4c61c16da4204aad2a761019
siravor <luis.sr@aluno.ifsc.edu.br>
2020-06-22 16:47:04 -0300

    update

---
+++ front-end/design-system/components/color-card/color-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pixel-color-card',
+  templateUrl: './color-card.component.html',
+  styleUrls: ['./color-card.component.scss']
+})
+export class ColorCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ front-end/design-system/components/color-list/color-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pixel-color-list',
+  templateUrl: './color-list.component.html',
+  styleUrls: ['./color-list.component.scss']
+})
+export class ColorListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
