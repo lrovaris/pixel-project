599b22b660f4770b73153b5168b3d4217fd5a14e
ciochetta <luisfelipeciochetta@gmail.com>
2020-06-29 16:38:59 -0300

    fix sprite save

--- front-end/src/main/save-sprite.js
+++ front-end/src/main/save-sprite.js
@@ -2,8 +2,10 @@ const fs = require('fs');
 
 async function save_file(name, data) {
 
+  name = name.replace('.pxl','');
+
   try {
-    fs.writeFileSync(`${name}.json`, JSON.stringify(data), 'utf-8');
+    fs.writeFileSync(`${name}.pxl`, JSON.stringify(data), 'utf-8');
 
     return {
       message: "Arquivo salvo com sucesso!"
