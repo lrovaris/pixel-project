71bb8cdcc918ad554652b6d4bf70b8198626c48d
lrovaris <luis.gabrielrovaris@gmail.com>
2020-08-14 09:37:05 -0300

    update front-end

---
+++ webApp/src/app/pages/tileset-page/tileset-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tileset-page',
+  templateUrl: './tileset-page.component.html',
+  styleUrls: ['./tileset-page.component.scss']
+})
+export class TilesetPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
